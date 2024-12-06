c4a4d510f1fbb9b91ad38299a267391fa74e3bee
Ravid-jam <ravid.jam02@gmail.com>
2024-12-06 11:38:56 +0530

    port added

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
 const express = require("express");
 var cors = require("cors");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-// const connectDB = require("./database/db");
+const connectDB = require("./database/db");
 const authRoutes = require("./routes/auth");
 const merchant = require("./routes/merchant");
 const employee = require("./routes/employee");
 const employeeWork = require("./routes/employeeWork");
+const authenticateToken = require("./middleware/auth");
+require("dotenv").config();
 
 const app = express();
-require("dotenv").config();
 app.use(cors());
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-    process.exit(1); // Exit the process if MongoDB connection fails
-  });
-// Middleware
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+connectDB();
+app.use(function (req, res, next) {
+  res.json({
+    message: "Hello , welcome to backend",
+    status: 200,
+  });
+});
 app.use("/auth", authRoutes);
 app.use("/merchants", merchant);
 app.use("/employee", employee);
 app.use("/employeeWork", employeeWork);
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello, welcome to the backend",
-    status: 200,
-  });
-});
-app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  res.status(err.status || 500);
-  res.render("error");
+app.get("/protected", authenticateToken, (req, res) => {
+  res.json({ message: "This is a protected route", user: req.user });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
 });
