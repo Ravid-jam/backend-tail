f49b081c06318e0685d025160060420e0f9a3c12
Ravid-jam <ravid.jam02@gmail.com>
2024-12-06 11:42:37 +0530

    port added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 var cors = require("cors");
 const bodyParser = require("body-parser");
-const connectDB = require("./database/db");
+// const connectDB = require("./database/db");
 const authRoutes = require("./routes/auth");
 const merchant = require("./routes/merchant");
+const mongoose = require("mongoose");
 const employee = require("./routes/employee");
 const employeeWork = require("./routes/employeeWork");
 const authenticateToken = require("./middleware/auth");
@@ -14,8 +15,8 @@ app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-connectDB();
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("connected"));
+// connectDB();
 app.use(function (req, res, next) {
   res.json({
     message: "Hello , welcome to backend",
